00998ecc739d560abae2072d8c2f34f50feae2b2
Luke Hannigan <lukeyhannigany@gmail.com>
2022-11-12 13:53:18 +0000

    contract address of bridge contract

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function main() {
     const app = express()
     app.use(express.json())
     app.get("/bridges", (req,res) => {
-        res.json(bridges.map(bridge => ({name: bridge.name, queue: bridge.queued})))
+        res.json(bridges.map(bridge => ({name: bridge.name, queue: bridge.queued, contractAddress: bridge.contract.address})))
     })
     app.post("/queueRequest", async (req, res) => {
         const { sourceChain, id } = req.body
